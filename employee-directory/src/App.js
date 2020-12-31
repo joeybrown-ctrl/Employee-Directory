875bb9d07afe528fc9342751261c9d2caf103885
Joey Brown <gjoey.brown@gmail.com>
2020-12-30 17:18:53 -0800

    added in form and started laying out the api call

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from "./form/Form.js";
 
 function App() {
   return (
+
     <div className="App">
+      <Form />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
