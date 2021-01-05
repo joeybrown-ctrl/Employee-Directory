ed3cb768aedd0e43c8ba9df32d60b418750198bb
Joey Brown <gjoey.brown@gmail.com>
2021-01-05 13:46:11 -0800

    added table

--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -1,18 +1,34 @@
-
-// import Table from "react-bootstrap/Table";
+import Table from "react-bootstrap/Table";
 
 function EmployeeTable(props) {
     const users = props.users;
 
     return (
-        // <Table striped bordered hover variant="dark">
+        // <h2>
+        //     {users.length > 0 ? users[0].name.first : ""}
+        //     Hello {console.log(users)}
+        // </h2>
 
-        // </Table>
-        <h2>
-            {/* {users[0].name.first} */}
-            {users.length > 0 ? users[0].name.first : ""}
-            Hello {console.log(users)}
-        </h2>
+        <Table striped bordered hover sizr="sm">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>username</th>
+                </tr>
+            </thead>
+            <tbody>
+                {this.state.results.map((result, i) => (
+                    <tr>
+                        <td>{i + 1}</td>
+                        <td>{result.name.first}</td>
+                        <td>{result.name.last}</td>
+                        <td>{result.login.username}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
     )
 }
 
