4532f3a526cd4a4e2dc35185cc5b9a844def5eb6
Joey Brown <gjoey.brown@gmail.com>
2021-01-06 10:16:07 -0800

    fixed nav again

--- src/components/EmpItem.js
+++ src/components/EmpItem.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Table from "react-bootstrap/Table";
+// import Table from "react-bootstrap/Table";
 
 function EmpItem(props) {
     return (
-        <tr className="table-item">
-            <th scope="row">{props.name}</th>
+        <tr>
+            <th>{props.name}</th>
             <td>{props.phone}</td>
             <td>{props.email}</td>
             <td><img src={props.picture} alt={props.name}></img></td>
